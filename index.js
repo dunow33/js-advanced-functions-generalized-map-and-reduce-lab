466a1d3aa897bf4326e13c430945fd22109d69b5
dunow <dunow33@hotmail.com>
2021-02-23 15:35:08 -0500

    all functions finished

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Add your functions here
+function map(arr, func){
+    return arr.map(element => func(element));
+}
+
+function reduce(src, callBack, start){
+    let result = (!!start) ? start : src[0];
+  
+    for (let i = (!!start) ? 0 : 1; i < src.length; i++) {
+      result = callBack(src[i], result);
+    }
+  
+    return result;
+  }
